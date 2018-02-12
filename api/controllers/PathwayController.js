650b961392b9ae4cfb063c72ec610dd51948adfa
carlo <crl.milesi@gmail.com>
2018-02-12 17:43:56 +0100

    script open data

--- api/controllers/PathwayController.js
+++ api/controllers/PathwayController.js
@@ -1,5 +1,12 @@
 exports.insertPathway =insertPathway;
 
+exports.get = get;
+exports.getJson = getJson;
+exports.getFileCsv = getFileCsv;
+exports.getFileJson = getFileJson;
+
+//exports.getGeoJson = getGeoJson;
+
 
 var validator = require('validator');
 var eventsProxy = require("../" + config.base.pathFactory + '/EventsFactory.js');    
@@ -50,6 +57,110 @@ function insertPathway (req, res)
                     
 
 }
+
+
+
+
+
+
+function get(req, res) {
+
+    var _limit = 50;
+    
+    if (req.query.limit) _limit = req.query.limit;     
+    
+    var data = {};
+    
+    sql.sequelize.query("select data from dat_pathway limit $limit  ",
+                            {
+                                bind: { 'limit': _limit }
+                                , type: sql.sequelize.QueryTypes.SELECT
+                            })
+                            .then(function (result) {
+                                
+                                data.city = 'cagliari';
+                                data.title = result[0].data.title;
+                                data.data_ins = result[0].data.dataIns;
+                                data.duration = result[0].data.duration;
+                                data.difficulty = result[0].data.difficolta;
+                                data.start_point = {};
+                                data.start_point.title = result[0].data.puntoPartenza;
+                                data.start_point.lat = result[0].data.start.lat;
+                                data.start_point.lng = result[0].data.start.lng;
+                                data.end_point = {};
+                                data.end_point.title = result[0].data.puntoPartenza;
+                                data.end_point.lat = result[0].data.stop.lat;
+                                data.end_point.lng = result[0].data.stop.lng;
+                                data.points = {};
+
+                                points_arr = [];
+                                for (i = 0; i < result[0].data.points.length; i++)
+                                {
+                                    points_arr.push({'title': result[0].data.points[i].title
+                                    ,'category': result[0].data.points[i].category
+                                    , 'lat': result[0].data.points[i].lat
+                                    , 'lng': result[0].data.points[i].lat
+                                    });
+
+                                
+                                }
+
+                                data.points = points_arr;
+                                
+
+
+                                return res.json(data);
+                                
+                                
+                            
+                            
+                            
+                            
+                            })
+                            .catch(function (ex) {
+                                console.log(ex);
+                                reject(getError("insert pathway", ex));
+                            });  
+    
+    
+    
+    }
+    
+    
+    function getJson(req, res) {
+    
+    var fs = require('fs');
+    
+    fs.readFile('./public/pathway.json', 'utf8', function(err, data){
+      //console.log(res.json(data));
+      
+    res.writeHeader(200, {"content-Type": "application/json"});
+    res.write(data);
+    res.end();
+    
+      //return res.json(res.json(data));
+    });
+    }
+    
+    function getFileJson(req, res) {
+    
+      var file = './public/pathway.json';
+      res.download(file);
+    
+    }
+    
+    
+    function getFileCsv(req, res) {
+    
+    
+      
+      var file = './public/pathway.csv';
+      
+    
+      res.download(file);
+      
+    }
+
 
 
 
---
+++ etl_script/script_create_csv.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+
+
+/************* Main ***********************/
+var json2csv = require('json2csv');
+var fs = require('fs');
+var Sequelize = require("sequelize");
+      
+
+var args = process.argv.slice(2);
+
+if (!args[0])
+{
+    console.log("no arguments!");
+}
+
+var config = {
+        "username": "postgres",
+            "password": "postgres_62",
+            "database": "seitre_tour_planner",
+            "options": {
+                "host": "156.148.14.146",
+                "port": 3998,
+                "dialect": "postgres",
+                "logging": false
+            }
+      };
+      
+      var sequelize = new Sequelize(config.database, config.username, config.password, config.options);
+      var db = {};
+      db.sequelize = sequelize;
+      
+    
+db.sequelize.query("select data from dat_"  + args[0],
+                            {
+                                type: db.sequelize.QueryTypes.SELECT
+                            })
+                            .then(function (result) {
+                                
+                                var data = {};
+
+                                data.city = 'cagliari';
+                                data.title = result[0].data.title;
+                                data.data_ins = result[0].data.dataIns;
+                                data.duration = result[0].data.duration;
+                                data.difficulty = result[0].data.difficolta;
+                                data.start_point = {};
+                                data.start_point.title = result[0].data.puntoPartenza;
+                                data.start_point.lat = result[0].data.start.lat;
+                                data.start_point.lng = result[0].data.start.lng;
+                                data.end_point = {};
+                                data.end_point.title = result[0].data.puntoPartenza;
+                                data.end_point.lat = result[0].data.stop.lat;
+                                data.end_point.lng = result[0].data.stop.lng;
+                                data.points = {};
+
+                                points_arr = [];
+                                for (i = 0; i < result[0].data.points.length; i++)
+                                {
+                                    points_arr.push({'title': result[0].data.points[i].title
+                                    ,'category': result[0].data.points[i].category
+                                    , 'lat': result[0].data.points[i].lat
+                                    , 'lng': result[0].data.points[i].lat
+                                    });
+
+                                
+                                }
+
+                                data.points = points_arr;
+                                db.sequelize.close();
+
+                                
+                                var fields = ['city',
+                                'title',
+                                'data_ins',
+                                'duration',
+                                'difficulty',
+                                'start_point',
+                                'end_point',
+                                'points'];
+          
+              var csv = json2csv({ data: data, fields: fields });
+           
+              fs.writeFile('../public/' + args[0] +'.csv', csv, function(err) {
+                if(err) 
+                    return console.log(err);
+                else
+                    return console.log("The file was saved!");
+              });                            
+
+    
+    
+}) 
+.catch(function (ex) {
+      console.log(ex);
+      return res.json({err: "get -> db error"});
+});
+
+ 
